4fbe870a6374f08e61978c6c450f7e115ffcf924
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2024-01-16 13:42:20 -0600

    interacting components

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const searchImage = async () => {
+const searchImage = async (term) => {
 
-    const response = await axios.get('http://api.unsplash.com/search/photos', {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID zmHL1-pgeaO026vx2IcQChHPj6aGZAv8pedt9w-AXME'
         },
 
         params: {
-            query: "cars"
+            query: term,
         }
 
 
     });
-    console.log(response);
-
-    return response;
+   
+    //to have more specific response write what you want to print from console when opening 
+    return response.data.results;
 
 };
 
